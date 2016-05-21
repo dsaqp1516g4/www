b9bfe626bacbf996b6cf72734dc5e533aa14e4a2
Joan <joanjerezt@gmail.com>
2016-05-21 21:27:32 +0200

    21h

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
 var BASE_URI = "http://localhost:8080/music4you"
+var WEBSERVER = "./img/"
 
 $(function(){
     
@@ -83,7 +84,7 @@ function logout(complete){
     	sessionStorage.removeItem("auth-token");
     	complete();
   	}).fail(function(){
-            alert.window("Falló el cierre de sesión");
+            window.alert("Falló el cierre de sesión");
         });
 } 
 
@@ -224,30 +225,24 @@ function getAnuncio(uri){
     }).done(function(data, status, jqxhr){
         var anuncio1 = data;
         $( "#anuncio_subject" ).replaceWith('<h2 id="anuncio_subject" class="modal-title" style="color:black">'+anuncio1.subject+'</h2>');
-        //$( "div.anuncio" ).replaceWith("<div><p>Creator:"+anuncio1.creator+"<br>Precio:"+anuncio1.price);
-        
-        
-        
-        
-        
-        //$('<div>' + anuncio1.subject +'</div>').appendTo($('#anuncio_subject'));
-        //$('<p style="background-color:#333">').appendTo($('#anuncio'));
-        //$('<strong>Creator: </strong>' + anuncio1.creator + '<br>').appendTo($('#anuncio'));
-        //$('<strong>Precio: </strong>' + anuncio1.precio + '€ <br>').appendTo($('#anuncio'));
         if(anuncio1.type=1){
                 var tipo="artista";
         }
         else{
                 var tipo="registrado";
         }
-        //$('<strong>Usuario: </strong>' + tipo + '<br>').appendTo($('#anuncio'));
-        //$('</p>').appendTo($('#anuncio'));
-        $('<p id="comment_result"></p>').appendTo($('#anuncio'));
-        data.links=linksToMap(data.links);
+        $("#anuncio").replaceWith(  '<p>'+ 
+                                  '<img align="right" src="' + WEBSERVER + anuncio1.image + '.png"><br clear="left"/></img>' 
+                                  + anuncio1.description + '</p><br>' + '<strong>Creator:</strong>' + anuncio1.creator 
+                                  + '<br>' + '<strong>Precio</strong>' + anuncio1.price + ' €'
+                                    + '<br>' + '<strong>Tipo de usuario</strong>' + tipo + '</p>'
+                                 + '<p id="comment_result"></p>');
+        loadComentarios(-1,anuncio1.id);
+        //data.links=linksToMap(data.links);
     }).fail(function(){
         console.log("ERROR");
         $("#anuncio").text("");
-        $("#anuncio").append("<div class='alert alert-block alert-danger'><p>Algo falló :(</p></div>");
+        $("#anuncio").replaceWith("<div class='alert alert-block alert-danger'><p>Algo falló :(</p></div>" + '<p id="comment_result"></p>');
     });
 }
 
@@ -280,11 +275,9 @@ function getAnuncio(uri){
                                         $('</p>').appendTo($('#event_result'));
                                         $('<a href="#" id="'+i+'event" class="list-group-item" data-toggle="modal" data-target="#VerEvento">Más...</a>').appendTo($('#event_result'));
                                         $("#"+i+"event").click(function(){
-                                            //event.preventDefault();
-                                            console.log("ID:" + evento.id);
-                                            getEvent("BASE_URI"+"/events/"+evento.id, function(){
-    
-                                            }); 
+                                        //event.preventDefault();
+                                        console.log("ID:" + evento.id);
+                                        getEvent(BASE_URI+"/events/"+evento.id); 
                                         });
 				});
         
@@ -300,6 +293,32 @@ function getAnuncio(uri){
         $("#event_result").append("<div class='alert alert-block alert-danger'><p>Algo falló :(</p></div>");
     });
 }
+
+function getEvent(uri){
+    var authToken = JSON.parse(sessionStorage["auth-token"]);
+    $.ajax({
+        url: uri,
+        type: 'GET',
+        crossDomain: true,
+        dataType: "json",
+        headers: {"X-Auth-Token" : authToken.token}
+    }).done(function(data, status, jqxhr){
+        var event1 = data;
+        $( "#event_subject" ).replaceWith('<h2 id="event_subject" class="modal-title" style="color:black">'+event1.titol+'</h2>');
+        $("#event").replaceWith('<div id="event">'+ event1.text + '<br>' + 
+                                '<strong>Fecha de comienzo:</strong>' + event1.startDate + '<br>' + 
+                                '<strong>Fecha de final</strong>' + event1.endDate + '</div>');
+        
+        loadComentarios(event1.id,-1);
+        //data.links=linksToMap(data.links);
+    }).fail(function(){
+        console.log("ERROR");
+        $("#event").text("");
+        $("#event").replaceWith("<div class='alert alert-block alert-danger'><p>Algo falló :(</p></div>" + '<p id="comment_result"></p>');
+    });
+}
+
+
 
 /*
  * USUARIOS *
@@ -337,4 +356,69 @@ function registrarUsuario (loginid, password, fullname, email, complete){
  *  COMENTARIOS *
  *              */
 
+function loadComentarios(eventid, anuncioid){
+    $('#comments').text('');
+    var authToken = JSON.parse(sessionStorage["auth-token"]);
+    if(eventid==-1){
+    var uri = BASE_URI+"/comments?"+"anuncioid="+anuncioid;
+    }
+    else{
+    var uri = BASE_URI+"/comments?"+"eventid="+eventid;
+    }
+    $.ajax({
+        type : 'GET',
+        url : uri,
+        headers: {"X-Auth-Token":authToken.token},
+        dataType : 'json',
+        crossDomain : true,
+    }).done(function(data, status, jqxhr){
+        
+        var comentarios = data.comments;
+        $.each(comentarios, function(i, v) {
+                                    var comentario = v;
+                                    console.log(comentario.content);
+                                    $('#comentario_result').replaceWith('<div id="comment_result"><h5>Comentarios</h5>' + comentario.content + ' por ' + comentario.creator+ ' el ' + comentario.fecha + '<img src="./img/user-trash.png" onclick="borrarComment(' + comentario.id + ');" style="cursor:pointer"></img>' +  '<img src="editar.png" onClick="editComment(' + comentario.id +')"></img></div>');
+                                    
+                                    //$('<div class="list-group"><a href="#" id="'+i+'anuncio" class="list-group-item" data-toggle="modal" data-target="#VerAnuncio"><h4 class="list-group-item-heading">' + anuncio.subject +'</h4></a>').appendTo($('#anuncio_result'));
+                                    //$('<p class="list-group-item-text">').appendTo($('#anuncio_result'));
+                                    //$('<strong>Userid: </strong>' + anuncio.userid + '<br>').appendTo($('#anuncio_result'));
+                                    //$('<strong>Precio: </strong>' + anuncio.precio + ' € <br>').appendTo($('#anuncio_result'));
+
+                                    //$('</p>').appendTo($('#anuncio_result'));
+                                    //$("#"+i+"anuncio").click(function(){
+                                    //event.preventDefault();
+                                    //console.log("ID:" + anuncio.id);
+                                    //getAnuncio(BASE_URI+"/anuncio/"+anuncio.id, function(){
+    
+                                    //});
+        });
+        //data.links=linksToMap(data.links);
+    }).fail(function(jqXHR, textStatus){
+        $("#comment_result").text("");
+        $("#comment_result").replaceWith("<div class='alert alert-block alert-danger'><p>Algo falló :(</p></div>");
+    });
+}
+
+function borrarComment(id){
+    var authToken = JSON.parse(sessionStorage["auth-token"]);
+    var uri=BASE_URI+"/comments/"+id;
+    $.ajax({
+        url: uri,
+        type: 'DELETE',
+        crossDomain: true,
+        dataType: "json",
+        //data: { content: contenido },
+        headers: {"X-Auth-Token":authToken.token}
+        
+    }).done(function(data, status, jqxhr){
+        //data.links=linksToMap(data.links);
+        //window.location.reload();
+    }).fail(function(){
+        console.log('Error');
+    });
+}
+
+/*
+ * PLAYLIST  *
+ *           */
 
